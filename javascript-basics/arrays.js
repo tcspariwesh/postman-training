0a9357773a92a29a73a4b49e8817555f2821d728
Pariwesh Gupta <pariwesh.gupta@publicissapient.com>
2025-12-15 11:58:15 +0530

    methods of arrays

---
+++ javascript-basics/arrays.js
@@ -0,0 +1,32 @@
+
+
+function sorting() {
+    const numbers = [1, 2, 3, 11, 4, 5];
+    numbers.sort();//treats elements as strings
+    numbers.sort(function(a, b) {//comparator function
+        console.log(a, b);
+        return a - b;
+    });
+    console.log(numbers);
+}
+sorting();
+
+
+function example1() {
+    const numbers = [1, 2, 3, 4, 5];
+    //add element
+    numbers.push(6);
+    numbers.push('Pariwesh');
+    console.log(numbers); // [1, 2, 3, 4, 5, 6, 'Pariwesh']
+    //remove last element
+    console.log(numbers[6]); //7 means 8th element
+    numbers.pop();
+    numbers.pop();
+    console.log(numbers);
+
+    //access element
+    console.log(numbers[7]); //7 means 8th element
+    //sorting
+    numbers.sort();
+    console.log(numbers);
+}
